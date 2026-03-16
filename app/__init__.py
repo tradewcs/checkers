614310d1db9161a3341360650c3a84b3fa686b1e
Mike <tradewcs@gmail.com>
2026-03-16 20:58:41 +0200

    Add __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from app.piece import (
+    Color,
+    PieceType,
+    Piece
+)
+from app.move import Move
+from app.board import (
+    Direction,
+    Node,
+    Board,
+)
+from app.coordinates import Coordinates
+
+__all__ = [
+    "Color",
+    "PieceType",
+    "Piece",
+    "Move",
+    "Direction",
+    "Node",
+    "Board",
+    "Coordinates",
+]
